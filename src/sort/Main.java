e87fccafecac9196ce40ff8e84f2c7961f4d880e
rgbli <2362995697@qq.com>
2020-12-09 09:47:05 +0800

    2020.12.9 添加快速排序

--- src/sort/Main.java
+++ src/sort/Main.java
@@ -1,7 +1,7 @@
 package sort;
 
 public class Main {
-    private static int[] arr = {1,8,4,3,7,2,9,6,5};
+    private static int[] arr = {1,6,4,3,7,2,9,8,5};
     private static int n = arr.length;
 
     // 冒泡排序
@@ -44,7 +44,7 @@ public class Main {
         }
 
         // 调整堆结构+交换堆顶元素与末尾元素
-        for (int i = arr.length - 1; i > 0; i--) {
+        for (int i = arr.length - 1; i >= 0; i--) {
             //将堆顶元素与末尾元素进行交换
             int temp = arr[i];
             arr[i] = arr[0];
@@ -76,8 +76,35 @@ public class Main {
     }
 
 
+    // 快速排序
+    public static void quickSort(int arr[], int left, int right) {
+        // 递归出口，这里不能写成 left != right
+        if (left < right) {
+            int i, j, tmp;
+            tmp = arr[left];
+            i = left;
+            j = right;
+            while (i != j) {
+                // 这里没有等于号，而且不要忘了i!=j的大条件
+                while (arr[j] > tmp && i != j){
+                    j--;
+                }
+                arr[i] = arr[j];
+
+                while (arr[i] < tmp && i != j) {
+                    i++;
+                }
+                arr[j] = arr[i];
+            }
+            arr[i] = tmp;     // tmp的操作类似插入排序
+            quickSort(arr, left, i - 1);
+            quickSort(arr, i + 1, right);
+        }
+    }
+
+
     public static void main(String[] args) {
-        heapSort(arr);
+        quickSort(arr, 0, arr.length - 1);
         for (int i : arr) {
             System.out.println(i);
         }
