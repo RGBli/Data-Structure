01248e8d59d1a10fe776e19afeb14260577ccec8
lbw <2362995697@qq.com>
2021-01-02 23:18:38 +0800

    2020.1.2 添加递归合并升序链表

--- src/linklist/ListNode.java
+++ src/linklist/ListNode.java
@@ -175,42 +175,19 @@ public class ListNode {
         return dummy.next;
     }
 
-    // 合并两个升序链表
+    // 递归合并两个升序链表
     public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        if (l1 == null) {
+        if (l1 == null) {   // 边界条件1
             return l2;
-        } else if (l2 == null) {
+        } else if (l2 == null) {    // 边界条件2
             return l1;
-        }
-
-        ListNode res = new ListNode();
-        ListNode tmp = res;
-        while (l1 != null && l2 != null) {
-            if (l1.val <= l2.val) {
-                tmp.next = new ListNode(l1.val);
-                tmp = tmp.next;
-                l1 = l1.next;
-            } else {
-                tmp.next = new ListNode(l2.val);
-                tmp = tmp.next;
-                l2 = l2.next;
-            }
-        }
-
-        if (l1 == null) {
-            while (l2 != null) {
-                tmp.next = l2;
-                tmp = tmp.next;
-                l2 = l2.next;
-            }
+        } else if (l1.val <= l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
         } else {
-            while (l1 != null) {
-                tmp.next = l1;
-                tmp = tmp.next;
-                l1 = l1.next;
-            }
+            l2.next = mergeTwoLists(l2.next, l1);
+            return l2;
         }
-        return res.next;
     }
 
     // 循环右移 k 位
